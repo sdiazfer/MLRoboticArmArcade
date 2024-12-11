b025bd5600d0c99a8e0c0f4bcfbab1319f48d58c
yashStevens <yfaldu@stevens.edu>
2024-12-10 22:39:03 -0500

    Bug fixes, UI improvements
    updated UI to look nicer
    Fixed bugs with pricesphere not collecting properly.

--- gui.py
+++ gui.py
@@ -9,7 +9,7 @@ class GUIManager:
         self.caught_count = 0
         self.arm = arm
         # create the canvas and adjust the camera
-        self.scene = canvas(title="Arcade Robotic Arm Simulator", width=800, height=600)
+        self.scene = canvas(title="Arcade Robotic Arm Simulator", width=800, height=500)
         self.scene.camera.pos = vector(25, -25, 25)
         self.scene.camera.axis = vector(-25, 25, -25)
         self.scene.up = vector(0, 0, 1)
@@ -20,20 +20,19 @@ class GUIManager:
         self.sliders = []  # List to hold sliders
         self.priceSphere = []  # List to hold price sphere objects
         self.prices = []  # List to hold the pos of the prices
+        self.prices2 = []  # List to hold the pos of the prices, Duplicate of list above
 
         self.setup_scene()  # Setting up the scene
         self.update_scene()  # Update positions after setup
-        # self.run_machine()
 
     def setup_scene(self):
         """
         Initialize the VPython 3D scene and robotic arm visualization.
         """
-        self.randomize_button = button(text="Randomize", bind=self.randomize)
-        self.reset_button = button(text="Reset", bind=self.reset_arm)
+        self.reset_button = button(text="Reset", bind=self.reset)
         self.generate_price_button = button(text="Generate Prices", bind=self.generate_price)
-        self.status = wtext(text=" Status: Ready\n")  # Status text next to buttons
         self.run_machine_button = button(text="Run Machine", bind=self.run_machine)
+        self.status = wtext(text=" Status: Ready\n")  # Status text next to buttons
 
         # Add a ground plane (a white flat plane)
         ground_plane = box(pos=vector(0, 0, -0.5), size=vector(100, 100, 0.1), color=color.white, opacity=0.6)
@@ -61,7 +60,7 @@ class GUIManager:
             wtext(text=f"Joint {i + 1}: ")
             slider_ctrl = slider(min=self.arm.joint_limit[i][0],
                                  max=self.arm.joint_limit[i][1],
-                                 length=200,
+                                 length=350,
                                  bind=self.update_angle,
                                  value=0,
                                  right=15
@@ -87,14 +86,16 @@ class GUIManager:
             link.axis = joint_pos[i + 1] - joint_pos[i]
 
         ee_pos = self.arm.dirKin(self.arm.joint_angles)
-        for i in range(len(self.prices)):
+        for i in range(len(self.prices) - 1, -1, -1):
             if self.prices[i].pickDet(ee_pos):
                 self.priceSphere[i].visible = False
+                self.priceSphere.pop(i)
+                self.prices.pop(i)
                 self.caught_count += 1
                 self.status.text = f" Status: You caught a prize! Total caught: {self.caught_count}\n"
 
-        if self.caught_count == len(self.prices):
-            self.status.text = "Congratulations! You caught all the prizes!"
+        if self.caught_count == 5:
+            self.status.text = "Congratulations! You caught all the prizes!\n"
 
     def update_angle(self, slider):
         """
@@ -104,53 +105,49 @@ class GUIManager:
         self.arm.joint_angles[idx] = radians(slider.value)
         self.update_scene()
 
-    def randomize(self, button=None):
-        """
-        Randomize the joint angles.
-        """
-        import random
-        for i in range(len(self.arm.joint_angles)):
-            self.arm.joint_angles[i] = random.uniform(-np.pi, np.pi)  # Random angle in radians
-            self.sliders[i].value = degrees(self.arm.joint_angles[i])  # Update slider values
-        self.update_scene()
-        self.status.text = " Status: Randomized"
-
-    def reset_arm(self, button=None):
+    def reset(self, button=None):
         """
         Reset the robotic arm to the home position.
         """
         self.arm.reset_arm()
+        self.caught_count = 0
         for slider in self.sliders:
             slider.value = 0
+        self.prices = self.prices2[:]
+        for price in self.priceSphere:
+            price.visible = False
+        self.priceSphere.clear()
+        for i in range(5):
+            self.priceSphere.append(sphere(pos=self.prices[i].pos, radius=1.5, color=color.green))
         self.update_scene()
         self.status.text = " Status: Reset\n"
-        for price in self.priceSphere:
-            price.visible = True
+
 
     def run_machine(self):
-        self.reset_arm()
+        self.reset()
+        self.status.text = ""
         seq = self.arm.sequence_planner(self.prices)
         ee_v = 1
         self.arm.trajectory_planner(seq, ee_v)
-        print("Trajectory Done")
 
         file = open("Trajectory.txt")
         for line in file:
             self.arm.joint_angles = list(map(float, line.strip().split(',')))
             self.update_scene()
             time.sleep(0.01)
+        file.close()
 
     def generate_price(self):
-        self.reset_arm()
         if self.priceGenerated:
             self.prices.clear()
             for price in self.priceSphere:
                 price.visible = False
-                del price
             self.priceSphere.clear()
+            self.reset()
 
         for i in range(5):
             temp = Price()
             self.prices.append(temp)
             self.priceSphere.append(sphere(pos=temp.pos, radius=1.5, color=color.green))
         self.priceGenerated = True
+        self.prices2 = self.prices[:]
